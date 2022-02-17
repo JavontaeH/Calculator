1f342fe7779817aa850e94ae8fc7e928aed640aa
JavontaeH <hardeman.javontae@gmail.com>
2022-02-17 13:25:15 -0600

    Simple functions for a calculator.

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const subtract = (number, number2) => {
+  const difference = number - number2;
+  return difference;
+};
+
+const divide = (number, number2) => {
+  const quotient = number / number2;
+  return quotient;
+};
+
+const multiply = (number, number2) => {
+  const product = number * number2;
+  return product;
+};
+
+const square = (number) => {
+  const squared = number * number;
+  return squared;
+};
+
+let answer = divide(60, 2.5);
+console.log(answer);
